80177bc885d947d2576b2546563be3a9473a324e
Your Name <rajansingh70707@gmail.com>
2025-07-13 23:43:11 +0530

    Added routes file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
 
 
 const ServerSetUp = async () => {
@@ -12,9 +15,16 @@ const ServerSetUp = async () => {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
 
+
+        app.use('/api', apiRoutes);
+
         app.listen(PORT, () => {
             console.log(`server is started at ${ PORT }`);
         });
+
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({ alter: true });
+        }
 }
 
 module.exports = ServerSetUp();
